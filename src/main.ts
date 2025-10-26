f31aab9113cecdba7d17dadcddc482f296fa5ae8
JunDevl <juninhoplay.pedone@gmail.com>
2025-10-26 17:36:15 -0300

    Improved styling and added some important elements to the UI

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,41 @@
 import "./style.css";
 
+enum View {
+	Temperature,
+	Wind,
+	Rain,
+}
+
+type TemperatureUnit = "celsius" | "feirenheit";
+
+const currentUnit: TemperatureUnit = "celsius";
+const currentView = View.Temperature;
+
 const location = document.querySelector("input.location") as HTMLInputElement;
-const date1 = document.querySelector("input.initial_date") as HTMLInputElement;
-const date2 = document.querySelector("input.final_date") as HTMLInputElement;
+const dateElem = document.querySelector(
+	"input.initial_date",
+) as HTMLInputElement;
 const result = document.querySelector("p.result") as HTMLParagraphElement;
 
 document.addEventListener("keypress", (e) => {
 	if (e.key !== "Enter") return;
 
-	let dates: Date[] = [];
+	const date = dateElem.value ? new Date(dateElem.value) : new Date();
 
-	if (!date1.value) {
-		dates = [new Date()];
-	} else {
-		dates = date2.value
-			? [new Date(date1.value), new Date(date2.value)]
-			: [new Date(date1.value)];
-	}
-
-	getWeatherData(location.value, dates).then((json) => {
-		result.textContent = JSON.stringify(json);
+	getWeatherData(location.value, date).then((json) => {
+		console.log(json);
+		//result.textContent = JSON.stringify(json);
 	});
 });
 
-async function getWeatherData(location: string, dates: Date[]) {
-	if (dates.length > 2)
-		throw new Error(
-			"Only two dates are allowed to be passed as arguments of this function.",
-		);
-
-	const date1 = Math.floor(Number(dates[0]) / 1000);
-	const date2 = dates[1] ? Math.floor(Number(dates[1]) / 1000) : date1;
-
+async function getWeatherData(location: string, date: Date) {
 	try {
+		const dateString = date.toISOString();
+
 		const weatherOfLocation = await fetch(
-			`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${date1}/${date2}?unitGroup=metric&key=${process.env.API_KEY}`,
+			`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${dateString}/${dateString}?unitGroup=metric&key=${process.env.API_KEY}`,
 		);
+
 		const weatherJson = await weatherOfLocation.json();
 
 		return weatherJson;
